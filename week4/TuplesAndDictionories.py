a7e0abf7332b9a8924a3ac97f303c21538c1cb08
SilentCodeSage <nandunandakishor345@gmail.com>
2024-08-20 20:46:13 +0530

    week4 tuples

---
+++ week4/TuplesAndDictionories.py
@@ -0,0 +1,16 @@
+#creating a dictionary
+#dictionary in py is object in js
+
+score = {}
+
+score["Test1"] = {}
+score["Test2"] = {}
+
+score["Test1"]["Nandaksihor"] = 100
+score["Test1"]["age"] = 20
+score["Test2"]["Nandakishor"] = 99
+score["Test2"]["place"] = "wayanad"
+
+print(score)
+print(score.keys())
+print(score.values())
